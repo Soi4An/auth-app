1eb0de8cb67b1a53603a4e36b6bd4d74cae24811
Antonio S <soi4.toxa@ukr.net>
2024-04-04 16:24:58 +0300

    tryed commenting dotenv code

--- server/src/index.js
+++ server/src/index.js
@@ -1,17 +1,17 @@
 'use strict';
 
-require('dotenv/config');
+// require('dotenv/config');
 
-const cors = require('cors');
+// const cors = require('cors');
 const express = require('express');
-const cookieParser = require('cookie-parser');
-const { corsOptions } = require('./utils/cors.js');
-const { authRouter } = require('./routers/auth.router.js');
-const { userRouter } = require('./routers/user.router.js');
-const { errorMW } = require('./middlewares/errorMW.js');
-const passport = require('passport');
-const session = require('express-session');
-const { googleRouter } = require('./routers/google.router.js');
+// const cookieParser = require('cookie-parser');
+// const { corsOptions } = require('./utils/cors.js');
+// const { authRouter } = require('./routers/auth.router.js');
+// const { userRouter } = require('./routers/user.router.js');
+// const { errorMW } = require('./middlewares/errorMW.js');
+// const passport = require('passport');
+// const session = require('express-session');
+// const { googleRouter } = require('./routers/google.router.js');
 
 const PORT = process.env.PORT || 3005;
 const app = express();
