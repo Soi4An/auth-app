e0033dc07d937c35c60b457ee360cbbd385fed76
Antonio S <soi4.toxa@ukr.net>
2024-04-04 19:19:42 +0300

    rewrite db.js

--- server/src/utils/cors.js
+++ server/src/utils/cors.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const corsOptions = {
-  origin: 'https://auth-app-client-vert.vercel.app/',
+  origin: 'https://auth-app-client-jaj3upaeu-anton-ss-projects-4d75c44a.vercel.app/',
   methods: 'GET,POST,PATCH',
   credentials: true,
 };
--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -4,14 +4,20 @@ require('dotenv/config');
 
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(process.env.POSTGRES_URL, {
-  dialect: 'postgres',
-  // host: process.env.POSTGRES_HOST || 'localhost',
-  // port: Number(process.env.POSTGRES_PORT) || 5432,
-  // database: process.env.POSTGRES_DB || 'postgres',
-  // username: process.env.POSTGRES_USER || 'postgres',
-  // password: process.env.POSTGRES_PASSWORD,
-  logging: false,
-});
+const db = new Sequelize(process.env.POSTGRES_URL);
+
+// const db = new Sequelize({
+//   connectionString: process.env.POSTGRES_URL,
+//   dialect: 'postgres',
+//   logging: false,
+// });
+
+// const db = new Sequelize(
+//  process.env.POSTGRES_UR,
+//  {
+//    dialect: 'postgres',
+//    logging: false,
+//  }
+// );
 
 module.exports = { db };
