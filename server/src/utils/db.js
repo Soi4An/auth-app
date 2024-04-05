c1c5b44c94b71aef3fdad8d5fd49ddce3392b8ab
Antonio S <soi4.toxa@ukr.net>
2024-04-05 09:13:18 +0300

    added dialectModule to db config

--- server/src/utils/db.js
+++ server/src/utils/db.js
@@ -4,7 +4,7 @@ require('dotenv/config');
 
 const { Sequelize } = require('sequelize');
 
-const db = new Sequelize(process.env.POSTGRES_URL);
+// const db = new Sequelize(process.env.POSTGRES_URL);
 
 // const db = new Sequelize({
 //   connectionString: process.env.POSTGRES_URL,
@@ -12,12 +12,10 @@ const db = new Sequelize(process.env.POSTGRES_URL);
 //   logging: false,
 // });
 
-// const db = new Sequelize(
-//  process.env.POSTGRES_UR,
-//  {
-//    dialect: 'postgres',
-//    logging: false,
-//  }
-// );
+const db = new Sequelize(process.env.POSTGRES_UR, {
+  dialect: 'postgres',
+  dialectModule: pg,
+  logging: false,
+});
 
 module.exports = { db };
