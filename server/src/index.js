b2be31ab6968b83bf78b7ec7871a7654ca73b423
Antonio S <soi4.toxa@ukr.net>
2024-04-11 18:01:29 +0300

    added corsOptions

--- server/src/index.js
+++ server/src/index.js
@@ -22,7 +22,7 @@ app.use(session({
   saveUninitialized: true,
 }));
 
-app.use(cors({credentials: false}));
+app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 app.use(passport.initialize());
